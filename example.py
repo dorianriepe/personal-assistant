9093e8c02d8c0a81b7d467a568b6de1d1710768e
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2021-02-18 23:49:37 +0100

    modified "example.py"

--- example.py
+++ example.py
@@ -2,6 +2,6 @@ from wrapper.google_calendar import Calendar
 
 calendar = Calendar("DHBW - Projekte")
 
-print("> Calendar ID: \t"+calendar.calendar_id+"\n")
-print("> Todays Events: \n"+str(calendar.get_events_today())+"\n")
-print("> First Event Tomorrow: \n"+str(calendar.get_first_event_tomorrow())+"\n")
+print("Calendar ID: \n"+calendar.calendar_id+"\n")
+print("Todays Events: \n"+str(calendar.get_events_today())+"\n")
+print("First Event Tomorrow: \n"+str(calendar.get_first_event_tomorrow())+"\n")
