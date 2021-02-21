b503956c65dfb3ee28c1932bec09813bc8f0629a
phisinger <philipsinger@mail.de>
2021-02-21 16:24:05 +0100

    adjustments and testing

--- wrapper/recipe_wrapper.py
+++ wrapper/recipe_wrapper.py
@@ -24,40 +24,43 @@ class RecipeWrapper:
     def getRecipeUrl(self):
         return self.recipe_url
     
+    def _get_response(self, querystring):
+        return requests.request("GET", self.recipe_url,params=querystring).json()
+    
     def get_recipe(self, ingredients, random_recipes=True, diet="balanced", health=None):
         
-        # ingredients: can be a list, or comma seperated string
+        # ingredients: comma seperated string
         # random_recipes: for multiple searches with same query
         # possible values for diet: balanced, high-protein, low-carb, low-fat
-        # possible values for health; alcohol-free, vegan, vegetarian, peanut-free, sugar conscious, tree-nut-free
+        # possible values for health; alcohol-free, vegan, vegetarian, peanut-free, sugar-conscious, tree-nut-free
         # full docu: https://developer.edamam.com/edamam-docs-recipe-api 
     
         querystring = {'app_id': "f19c07e5",
                        'app_key': "dc7784972462d1c8e4230d5a6d6fb3e1",
                        "q":ingredients, 
                        "diet": diet}
-        
         if health:
             querystring["health"] = health
         
-        response = requests.request("GET", self.recipe_url,params=querystring)
-        
-        if response:
-            response_json = response.json()
-            
+        response_json = self._get_response(querystring)
+
+        if response_json:
             recipe_number = 0
             if random_recipes:
                 recipe_number = randint(0,9)
-                
-            recipe = response_json['hits'][recipe_number]['recipe']
             
-            return {"recipe_name": recipe["label"], 
-                    "recipe_image": recipe["image"], 
-                    "recipe_url": recipe["url"],
-                    "recipe_calories": int(recipe["calories"]),
-                    "recipe_ingredients": recipe["ingredientLines"],
-                    "recipe_time": recipe["totalTime"]
-                    }
+            try:
+                recipe = response_json['hits'][recipe_number]['recipe']
+            
+                return {"recipe_name": recipe["label"], 
+                        "recipe_image": recipe["image"], 
+                        "recipe_url": recipe["url"],
+                        "recipe_calories": int(recipe["calories"]),
+                        "recipe_ingredients": recipe["ingredientLines"],
+                        "recipe_time": recipe["totalTime"]
+                        }
+            except:
+                return
                     
         else:
             print("An error occured")
