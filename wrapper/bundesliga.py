2cb9f503335d58568e43f543b3e4ab0fe8229cb1
Jan Mäder <jan-maeder@gmx.de>
2021-02-21 15:38:02 +0100

    response format implemented

--- wrapper/bundesliga.py
+++ wrapper/bundesliga.py
@@ -3,31 +3,47 @@ import requests
 import json
 
 class Bundesliga:
+    def __init__(self, club, liga):
+        if(liga == 1):
+            self.liga = 'bl1'
+        elif(liga == 2):
+            self.liga = 'bl2' 
+        else:
+            print('Error: No liga')
+        self.club = club
     
-    @staticmethod
-    def get_bundesliga_ergebnisse():
-        # response = requests.get('https://www.openligadb.de/api/getmatchdata/bl1')
-        response = requests.get('https://www.openligadb.de/api/getmatchdata/bl1/2020/21')
+
+    def get_bundesliga_ergebnisse(self):
+        spiele_dicts = []
+        # response = requests.get('https://www.openligadb.de/api/getmatchdata/%s' % self.liga)
+        response = requests.get('https://www.openligadb.de/api/getmatchdata/%s/2020/21' % self.liga)
+
         if response:
             spieltag = response.json()
 
-            spieltagNr = spieltag[0]['Group']['GroupName']
-            print("Die Ergebnisse des", spieltagNr, "Speiltags")
-
             for spiel in spieltag:
-                matchID = spiel['MatchID']
-                team1 = spiel['Team1']['ShortName']
-                team2 = spiel['Team2']['ShortName']
+                spiel_dict = {
+                    "liga": self.liga,
+                    "spieltag": spieltag[0]['Group']['GroupOrderID'],
+                    "matchID": spiel['MatchID'],
+                    "team1": spiel['Team1']['ShortName'],
+                    "team2": spiel['Team2']['ShortName']
+                }
 
                 if spiel['MatchIsFinished']:
-                    toreTeam1 = spiel['MatchResults'][0]['PointsTeam1']
-                    toreTeam2 = spiel['MatchResults'][0]['PointsTeam2']
-
-                    print( matchID, team1, ":", team2, toreTeam1, toreTeam2)
+                    spiel_dict["finished"] = True
+                    spiel_dict["toreTeam1"] = spiel['MatchResults'][0]['PointsTeam1']
+                    spiel_dict["toreTeam2"] = spiel['MatchResults'][0]['PointsTeam2']
 
                 else:
-                    print (matchID, team1, ":", team2, "noch nicht entschieden!!")
+                    spiel_dict["finished"] = False
+                    spiel_dict["toreTeam1"] = 0
+                    spiel_dict["toreTeam2"] = 0
 
+                spiele_dicts.append(spiel_dict)
+        
+            return spiele_dicts
+        
         else:
             print('An error has occurred.')
     
