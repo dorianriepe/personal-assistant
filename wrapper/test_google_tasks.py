eef0174e3b99971d15101aadeed33079cffa7124
dorianriepe <dorian.riepe@me.com>
2021-02-23 00:06:54 +0100

    test tasks wrapper

--- wrapper/test_google_tasks.py
+++ wrapper/test_google_tasks.py
@@ -24,19 +24,17 @@ class Testing(unittest.TestCase):
 
         self.assertEqual(response, response_exp)
     
-    '''
-    @patch('google_tasks.Tasks._get_events_for_day')
+    
+    @patch('google_tasks.Tasks._get_tasks')
     def test_todays_events(self, mock__get_response):
 
-        mock__get_response.return_value = [{'title': 'Datenbanken', 'location': 'https://dhbw-stuttgart.zoom.us/j/99643057619', 'start': '10:00', 'end': '12:30'}, {'title': 'Security', 'location': '', 'start': '13:15', 'end': '15:45'}, {'title': 'Zero Knowledge', 'location': '', 'start': '13:00', 'end': '15:30'}]
+        mock__get_response.return_value = {'kind': 'tasks#tasks', 'etag': '"LTg3MzE0MDkyOQ"'}
 
-        google_calendar = Calendar("DHBW6")
-        events = google_calendar.get_events_today()
+        tasks = Tasks()
+        response = tasks.get_tasks_from_list("Einkaufen")
 
-        response = [{'title': 'Datenbanken', 'location': 'https://dhbw-stuttgart.zoom.us/j/99643057619', 'start': '10:00', 'end': '12:30'}, {'title': 'Security', 'location': '', 'start': '13:15', 'end': '15:45'}, {'title': 'Zero Knowledge', 'location': '', 'start': '13:00', 'end': '15:30'}]
-
-        self.assertEqual(events, response)
-    '''
+        self.assertEqual(response, [])
+    
 
 if __name__ == '__main__':
     unittest.main()
