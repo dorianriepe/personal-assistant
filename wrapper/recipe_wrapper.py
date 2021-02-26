286faba396a0302266427b9c3dd0476b112c7ee9
phisinger <philipsinger@mail.de>
2021-02-26 17:45:38 +0100

    Add function to recipe-wrapper for usability of cooking use case

--- wrapper/recipe_wrapper.py
+++ wrapper/recipe_wrapper.py
@@ -29,7 +29,33 @@ class RecipeWrapper:
     def get_response(self, querystring):
         return requests.request("GET", self.recipe_url, params=querystring).json()
 
-    def get_recipe(self, ingredients, random_recipes=True, diet="balanced", health=None):
+    def get_recipe_by_uri(self, uri):
+        querystring = {"app_id": os.environ.get('RECIPE_APP_ID'),
+                       "app_key": os.environ.get('RECIPE_APP_KEY'),
+                       "r": uri}
+
+        response_json = self.get_response(querystring)
+
+        if response_json:
+            try:
+                recipe = response_json[0]
+
+                return {"recipe_uri": recipe["uri"],
+                        "recipe_name": recipe["label"],
+                        "recipe_image": recipe["image"],
+                        "recipe_url": recipe["url"],
+                        "recipe_calories": int(recipe["calories"]),
+                        "recipe_ingredients": recipe["ingredientLines"],
+                        "recipe_time": recipe["totalTime"]
+                        }
+            except:
+                return
+
+        else:
+            print("An error occured")
+            return
+
+    def get_recipe_by_ingredients(self, ingredients, random_recipes=True, diet="balanced", health=None):
 
         # ingredients: comma seperated string
         # random_recipes: for multiple searches with same query
@@ -54,7 +80,8 @@ class RecipeWrapper:
             try:
                 recipe = response_json['hits'][recipe_number]['recipe']
 
-                return {"recipe_name": recipe["label"],
+                return {"recipe_uri": recipe["uri"],
+                        "recipe_name": recipe["label"],
                         "recipe_image": recipe["image"],
                         "recipe_url": recipe["url"],
                         "recipe_calories": int(recipe["calories"]),
