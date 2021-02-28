fc172db9419beb79dd2565cb6aee59348f2dc490
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2021-02-28 16:18:33 +0100

    renamed class, file, fixed test demaged from merge conflict (#38)

--- wrapper/test_recipes.py
+++ wrapper/test_recipes.py
@@ -1,16 +1,17 @@
+import unittest
 from unittest import TestCase
 from unittest.mock import patch
 import json
 import os
 
-from recipe_wrapper import RecipeWrapper
+from recipes import Recipes
 
 
-class TestRecipeWrapper(TestCase):
+class TestRecipes(TestCase):
 
-    @patch('recipe_wrapper.RecipeWrapper.get_response')
+    @patch('recipes.Recipes.get_response')
     def test_get_recipe_by_ingredients1(self, mock_get_response):
-        inst = RecipeWrapper.getInstance('http://127.0.0.1')
+        inst = Recipes.getInstance('http://127.0.0.1')
         test_path = os.path.join("test-responses-recipes", "response1.json")
         file = open(test_path, "r")
         ml = json.loads(file.read())
@@ -26,10 +27,10 @@ class TestRecipeWrapper(TestCase):
 
         self.assertEqual(expected_res, res)
 
-    @patch('recipe_wrapper.RecipeWrapper.get_response')
+    @patch('recipes.Recipes.get_response')
     def test_get_recipe_by_ingredients2(self, mock_get_response):
 
-        inst = RecipeWrapper.getInstance('http://127.0.0.1')
+        inst = Recipes.getInstance('http://127.0.0.1')
         test_path = os.path.join("test-responses-recipes", "response2.json")
         file = open(test_path, "r")
         ml = json.loads(file.read())
@@ -37,16 +38,15 @@ class TestRecipeWrapper(TestCase):
 
         mock_get_response.return_value = ml
 
-        res = inst.get_recipe_by_ingredients("noodles, chicken, pepper, milk",
-                                             random_recipes=False, diet="low-fat")
-        expected_res = {'recipe_uri': 'http://www.edamam.com/ontologies/edamam.owl#recipe_696ea2fc73f79991174e2a7b650b48f9', 'recipe_name': 'Tuna Casserole', 'recipe_image': 'https://www.edamam.com/web-img/ea7/ea714deca9d0a2fc9067263de1d7db55.jpeg', 'recipe_url': 'https://www.foodnetwork.com/recipes/tuna-casserole-recipe-1938022', 'recipe_calories': 3317, 'recipe_ingredients': ['5 slices whole-wheat bread, crusts included', '1 tablespoon canola oil',
+        res = inst.get_recipe_by_ingredients("noodles, chicken, pepper, milk", random_recipes=False, diet="low-fat")
+        expected_res = {'recipe_uri': 'http://www.edamam.com/ontologies/edamam.owl#recipe_696ea2fc73f79991174e2a7b650b48f9', 'recipe_name': 'Tuna Casserole', 'recipe_image': 'https://www.edamam.com/web-img/ea7/ea714deca9d0a2fc9067263de1d7db55.jpeg', 'recipe_url': 'https://www.foodnetwork.com/recipes/tuna-casserole-recipe-1938022', 'recipe_calories': 3317, 'recipe_ingredients': ['5 slices whole-wheat bread, crusts included', '1 tablespoon canola oil','1 small onion, chopped (about 1 cup)', '1 large stalk celery, finely diced (about 1/2 cup)', '1 (10-ounce) box white mushroom, stemmed and chopped (about 2 1/2 cups)', '1/4 cup all-purpose flour', '3 cups 1 percent milk', '1 cup low-sodium chicken broth or vegetable broth', '3/4 teaspoon salt', '1/4 teaspoon ground black pepper', '1/2 pound whole-wheat fettuccine noodles, broken into thirds and cooked according to package directions', '1 (10-ounce) box frozen chopped broccoli, thawed', '1 (10-ounce) box frozen peas, thawed', '4 (6-ounce) cans chunk light tuna in water, drained'], 'recipe_time': 60.0}
 
 
         self.assertEqual(expected_res, res)
 
-    @patch('recipe_wrapper.RecipeWrapper.get_response')
+    @patch('recipes.Recipes.get_response')
     def test_get_recipe_by_ingredients3(self, mock_get_response):
-        inst = RecipeWrapper.getInstance('http://127.0.0.1')
+        inst = Recipes.getInstance('http://127.0.0.1')
         test_path = os.path.join("test-responses-recipes", "response3.json")
         file = open(test_path, "r")
         ml = json.loads(file.read())
@@ -59,9 +59,9 @@ class TestRecipeWrapper(TestCase):
 
         self.assertEqual(expected_res, res)
 
-    @patch('recipe_wrapper.RecipeWrapper.get_response')
+    @patch('recipes.Recipes.get_response')
     def test_get_recipe_by_ingredients4(self, mock_get_response):
-        inst = RecipeWrapper.getInstance('http://127.0.0.1')
+        inst = Recipes.getInstance('http://127.0.0.1')
         test_path = os.path.join("test-responses-recipes", "response4.json")
         file = open(test_path, "r")
         ml = json.loads(file.read())
@@ -75,19 +75,19 @@ class TestRecipeWrapper(TestCase):
 
         self.assertEqual(expected_res, res)
 
-    @patch('recipe_wrapper.RecipeWrapper.get_response')
+    @patch('recipes.Recipes.get_response')
     def test_get_recipe_by_uri1(self, mock_get_response):
-        inst = RecipeWrapper.getInstance('http://127.0.0.1')
+        inst = Recipes.getInstance('http://127.0.0.1')
         test_path = os.path.join("test-responses-recipes", "response5.json")
         file = open(test_path, "r")
         ml = json.loads(file.read())
         file.close()
 
         mock_get_response.return_value = ml
-        res = inst.get_recipe_by_uri(
-            "http://www.edamam.com/ontologies/edamam.owl#recipe_35a93fee16e7683b1ece4d9070f974f1")
-        expected_res = {'recipe_uri': 'http://www.edamam.com/ontologies/edamam.owl#recipe_35a93fee16e7683b1ece4d9070f974f1', 'recipe_name': 'Chicken, Spinach, And Noodle Casserole', 'recipe_image': 'https://www.edamam.com/web-img/bf8/bf8173103cd9cffd0da967b1cbb93636.jpg', 'recipe_url': 'http://www.realsimple.com/food-recipes/browse-all-recipes/chicken-noodle-casserole-00100000075552/index.html',
-                        'recipe_calories': 3902, 'recipe_ingredients': ['6 tablespoons unsalted butter', '1/4 cup all-purpose flour', '4 cups whole milk', '1 cup sour cream', 'kosher salt and black pepper', '12 ounces egg noodles', '4 slices sandwich bread', '2 cups shredded cooked chicken or rotisserie chicken', '5 ounces baby spinach, chopped', '2 teaspoons dried thyme'], 'recipe_time': 0.0}
+        res = inst.get_recipe_by_uri("http://www.edamam.com/ontologies/edamam.owl#recipe_35a93fee16e7683b1ece4d9070f974f1")
+        expected_res = {'recipe_uri': 'http://www.edamam.com/ontologies/edamam.owl#recipe_35a93fee16e7683b1ece4d9070f974f1', 'recipe_name': 'Chicken, Spinach, And Noodle Casserole', 'recipe_image': 'https://www.edamam.com/web-img/bf8/bf8173103cd9cffd0da967b1cbb93636.jpg', 'recipe_url': 'http://www.realsimple.com/food-recipes/browse-all-recipes/chicken-noodle-casserole-00100000075552/index.html','recipe_calories': 3902, 'recipe_ingredients': ['6 tablespoons unsalted butter', '1/4 cup all-purpose flour', '4 cups whole milk', '1 cup sour cream', 'kosher salt and black pepper', '12 ounces egg noodles', '4 slices sandwich bread', '2 cups shredded cooked chicken or rotisserie chicken', '5 ounces baby spinach, chopped', '2 teaspoons dried thyme'], 'recipe_time': 0.0}
 
         self.assertEqual(expected_res, res)
 
+if __name__ == '__main__':
+    unittest.main()
