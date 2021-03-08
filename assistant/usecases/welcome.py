927e4d17c5961b8632655bfd556c973c022ac4d7
0xfr34ky <51492796+0xfr34ky@users.noreply.github.com>
2021-03-08 18:04:45 +0100

    Implement welcome usecase with contexts [none, workout, spotify]

--- assistant/usecases/welcome.py
+++ assistant/usecases/welcome.py
@@ -1,16 +1,61 @@
 class Welcome:
 
-    def __init__(self):
-        self.context = None
+  def __init__(self):
+    self.context = None
 
-    def handle(self, text, context, preferences):
-        
-        response = {
-                "text": "Good morning, today it's going to be sunny 22 degrees. NASA's Perseverance rover has successfully touched down on Mars.",
-                "html": "<p>Good Morning!<br> today it's going to be sunny 22 degrees. NASA's \"Perseverance\" rover has successfully touched down on Mars. For more information check this website:<p><a href=\"https://www.tagesschau.de/ausland/mars-landung-101.html\">Rover \"Perseverance\" lands on Mars</a>",
-                "follow_up": "welcome",
-                "context": "test"
-            }
+  def handle(self, text, context, preferences):
+    if context == "workout":
+      return self.workout(text, preferences)
+    elif context == "spotify":
+      return self.spotify(text, preferences)
+    else:
+      return self.welcome(text, preferences)
 
-        return response
-        
+ def time_for_workout?(self, event):
+   if event.size
+     diff = datetime.strptime(event[0].start, '%H:%M') - datetime.now()
+     if diff.total_seconds() < 3600: # One hour
+       return False
+   return True
+
+ def welcome(self, text, preferences):
+   greetings = "Good morning."
+
+   weather = Weather.get_todays_forecast
+
+   articleList = NewsScrapper.getArticleList
+   news = articleList[0].description
+   news_preview = "<a href = "+ articleList[0].link +">" + articleList[0].title + "</a>"
+
+   events = Calendar.get_events_for_today
+   if events.size:
+     calendar = "Today you have no entries in your calendar."
+   else:
+     calendar = "Today you have " + str(events.size) + " lectures. The first starts at "+events[0].start + "."
+
+
+   text = greetings + " " + weather + " " + news + " " + calendar
+   html = "<p>" + greetings + " " + weather + "</p><p>" + news + "</p>" + news_preview + "<p>" + calendar + "</p>"
+   if self.time_for_workout?(events):
+     text += " Do you want to start a workout an listen to some music?"
+     html += "<p>Do you want to start a workout an listen to some music?</p>"
+     follow_up = "welcome"
+     context = "workout"
+   else:
+     follow_up = None
+     context = None
+
+   return { "text": text, "html": html, "follow_up": follow_up, "context": context }
+
+ def workout(self, text, preferences):
+   if text.lower.find(positive_text) >= 0:
+     answer = "<p>Ok. Should I start the playlist on Spotify?</p>"
+     spotify_preview = ""
+     return { "text": answer, "html": answer + spotify_preview, "follow_up": "welcome", "context": "spotify" }
+   else:
+     return { "text": None, "html": None, "follow_up": None, "context": None }
+
+  def spotify(self, text, preferences):
+    if text.lower.find(positive_text) >= 0:
+      #play spotify
+    return { "text": None, "html": None, "follow_up": None, "context": None }
