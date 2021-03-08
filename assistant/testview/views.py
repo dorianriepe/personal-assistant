ec801e08f1a54db965c08c8d0305bd986347d7df
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2021-03-08 22:32:43 +0100

    DR Frontend Redesign (#53)
    * first frontend redesign
    
    * minor design changes
    
    * designed ui elements
    
    * updated readme
    
    * updated README

--- assistant/testview/views.py
+++ assistant/testview/views.py
@@ -6,3 +6,7 @@ def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
     template = loader.get_template('index.html')
     return HttpResponse(template.render({}, request))
+
+def newindex(request):
+    template = loader.get_template('newindex.html')
+    return HttpResponse(template.render({}, request))
