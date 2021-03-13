b386a2c0d5dbc7c05b6fd16d830a8687fce5ce41
Nils Imhoff <github@d4d1.de>
2021-03-14 00:49:36 +0100

    Ni recipe (#84)
    * fix bug
    
    * fix bug

--- assistant/wrapper/test_weather.py
+++ assistant/wrapper/test_weather.py
@@ -13,7 +13,9 @@ class Testing(unittest.TestCase):
     def test_get_current_weather(self, mock__get_response):
 
         response_path = os.path.join("weather-response", "weather.json")
-        values = json.loads(open(response_path, "r").read())
+        fo = open(response_path, "r")
+        values = json.loads(fo.read())
+        fo.close()
         mock__get_response.return_value = values
 
         w = Weather("Stuttgart")
@@ -28,22 +30,25 @@ class Testing(unittest.TestCase):
     def test_get_forecast(self, mock__get_response, mock__time_response):
 
         response_path = os.path.join("weather-response", "weather.json")
-        values = json.loads(open(response_path, "r").read())
+        fo = open(response_path, "r")
+        values = json.loads(fo.read())
+        fo.close()
         mock__get_response.return_value = values
-        mock__time_response.return_value = datetime(year=2021, month=2, day=23, hour=14, minute=50)
+        mock__time_response.return_value = datetime(year=2021, month=2, day=23, hour=9, minute=00)
 
         w = Weather("Stuttgart")
         response = w.get_forecast()
 
-        expected_response = '15:00Partly cloudy16018:00Partly cloudy16021:00Clear1400:00Clear1203:00Clear1106:00Clear909:00Sunny11012:00Partly cloudy16015:00Sunny19018:00Clear17021:00Clear1400:00Clear1203:00Clear1106:00Clear909:00Sunny12012:00Sunny17015:00Sunny19018:00Sunny17021:00Clear140'
-
+        expected_response = "The weather this morning is Sunny at 11 degrees.\n"
         self.assertEqual(response, expected_response)
 
     @patch('weather.Weather.get_json_weather')
     def test_get_evening_forecast(self, mock__get_response):
 
         response_path = os.path.join("weather-response", "weather.json")
-        values = json.loads(open(response_path, "r").read())
+        fo = open(response_path, "r")
+        values = json.loads(fo.read())
+        fo.close()
         mock__get_response.return_value = values
 
         w = Weather("Stuttgart")
