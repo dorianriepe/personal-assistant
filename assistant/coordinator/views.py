aee12057daa1498304fb74d62ae01a1b8436e80a
Dorian Riepe <44470917+dorianriepe@users.noreply.github.com>
2021-03-29 13:13:25 +0200

    Update views.py

--- assistant/coordinator/views.py
+++ assistant/coordinator/views.py
@@ -101,50 +101,35 @@ def reminder(request):
         timzone_berlin = pytz.timezone('Europe/Berlin')
         now = datetime.now(timzone_berlin)
 
-        morning_default = datetime.strptime(
-            request.POST['morning_reminder'], '%H:%M')
-        shopping_default = datetime.strptime(
-            request.POST['shopping_reminder'], '%H:%M')
-        cooking_default = datetime.strptime(
-            request.POST['cooking_reminder'], '%H:%M')
-        evening_default = datetime.strptime(
-            request.POST['evening_reminder'], '%H:%M')
+        morning_default = datetime.strptime(request.POST['morning_reminder'], '%H:%M')
+        shopping_default = datetime.strptime(request.POST['shopping_reminder'], '%H:%M')
+        cooking_default = datetime.strptime(request.POST['cooking_reminder'], '%H:%M')
+        evening_default = datetime.strptime(request.POST['evening_reminder'], '%H:%M')
 
-        morning = now.replace(hour=morning_default.hour,
-                              minute=morning_default.minute)
-        shopping = now.replace(hour=shopping_default.hour,
-                               minute=shopping_default.minute)
-        cooking = now.replace(hour=cooking_default.hour,
-                              minute=cooking_default.minute)
-        evening = now.replace(hour=evening_default.hour,
-                              minute=evening_default.minute)
+        morning = now.replace(hour=morning_default.hour, minute=morning_default.minute)
+        shopping = now.replace(hour=shopping_default.hour, minute=shopping_default.minute)
+        cooking = now.replace(hour=cooking_default.hour, minute=cooking_default.minute)
+        evening = now.replace(hour=evening_default.hour, minute=evening_default.minute)
 
         calendar = Calendar("ASWE")
         todays_events = calendar.get_events_today()
 
         for event in todays_events:
-            event_start = datetime.strptime(
-                event['start'], '%H:%M') + timedelta(minutes=-10)
-            event_start = now.replace(
-                hour=event_start.hour, minute=event_start.minute)
-            reminders.append({"ts": event_start.strftime(
-                "%H:%M"), "hour": event_start.hour, "minute": event_start.minute, "usecase": "meeting"})
+            event_start = datetime.strptime(event['start'], '%H:%M') + timedelta(minutes=-10)
+            event_start = now.replace(hour=event_start.hour, minute=event_start.minute)
+            reminders.append({"ts": event_start.strftime( "%H:%M"), "hour": event_start.hour, "minute": event_start.minute, "usecase": "meeting"})
 
         if now < morning:
-            reminders.append({"ts": morning.strftime(
-                "%H:%M"), "hour": morning.hour, "minute": morning.minute, "usecase": "welcome"})
+            reminders.append({"ts": morning.strftime("%H:%M"), "hour": morning.hour, "minute": morning.minute, "usecase": "welcome"})
 
         if now < shopping:
-            reminders.append({"ts": shopping.strftime(
-                "%H:%M"), "hour": shopping.hour, "minute": shopping.minute, "usecase": "hungry"})
+            reminders.append({"ts": shopping.strftime("%H:%M"), "hour": shopping.hour, "minute": shopping.minute, "usecase": "hungry"})
 
         if now < cooking:
-            reminders.append({"ts": cooking.strftime(
-                "%H:%M"), "hour": cooking.hour, "minute": cooking.minute, "usecase": "hungry"})
+            reminders.append({"ts": cooking.strftime("%H:%M"), "hour": cooking.hour, "minute": cooking.minute, "usecase": "hungry"})
 
         if now < evening:
-            reminders.append({"ts": evening.strftime(
-                "%H:%M"), "hour": evening.hour, "minute": evening.minute, "usecase": "night"})
+            reminders.append({"ts": evening.strftime("%H:%M"), "hour": evening.hour, "minute": evening.minute, "usecase": "night"})
 
         reminders = sorted(reminders, key=lambda k: k['ts'])
 
